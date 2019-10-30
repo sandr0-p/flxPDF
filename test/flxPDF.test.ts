124796be83e771c020674e5e570e39c55f2221bc
Sandro Paetzold <Sandro_Paetzold@tjxeurope.com>
2019-10-30 16:40:18 +0000

    Add test

---
+++ test/flxPDF.test.ts
@@ -0,0 +1,9 @@
+import { before } from 'mocha';
+import { expect } from 'chai';
+import * as fs from 'fs';
+
+describe('flxPDF', () => {
+    it('should create a file when ArrayBuffer is provided', (done) => {
+        
+    });
+});
